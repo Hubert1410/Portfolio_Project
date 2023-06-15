70aab5b1f1737f41bb6d8acd943caf83818ba127
Hubert_Matysiak <94004104+Hubert1410@users.noreply.github.com>
2023-06-15 22:16:02 +0200

    Update renaming_files.py

--- renaming_files.py
+++ renaming_files.py
@@ -11,37 +11,37 @@ import pandas as pd
 import os
 import shutil
 
-# Ścieżka do pliku Excela
+# Path to the Excel file
 excel_path = "C:/Users/Desktop/Python/Data.xlsx"
 
-# Wczytanie pliku Excela
+# Read the Excel file
 df = pd.read_excel(excel_path)
 
-# Iteracja przez wiersze DataFrame
+# Iterate through the DataFrame rows
 for index, row in df.iterrows():
-    filename = str(row['A'])  # Wartość komórki A w danym wierszu
-    new_filename = row['B']  # Wartość komórki B w danym wierszu
+    filename = str(row['A'])
+    new_filename = row['B']
 
-    # Sprawdzenie, czy istnieje plik o nazwie zgodnej z wartością komórki A
+    # Check if a file with the name from cell A exists
     if os.path.isfile(filename):
-        # Tworzenie ścieżki do folderu "XCOPIED_STORES" na tej samej ścieżce
+        # Create a path for the "XCOPIED_STORES" folder in the same directory
         folder_path = os.path.join(os.path.dirname(filename), "XCOPIED_STORES")
 
-        # Sprawdzenie, czy folder "XCOPIED_STORES" nie istnieje
+        # Check if the "XCOPIED_STORES" folder doesn't exist
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)
 
-        # Tworzenie ścieżki docelowej dla skopiowanego pliku
+        # Create the destination path for the copied file
         new_filepath = os.path.join(folder_path, os.path.basename(filename))
 
-        # Kopiowanie pliku do folderu "XCOPIED_STORES"
+        # Copy the file to the "XCOPIED_STORES" folder
         shutil.copy2(filename, new_filepath)
 
-        # Zmiana nazwy skopiowanego pliku zgodnie z wartością komórki B
+        # Rename the copied file according to the value in cell B
         new_filepath_renamed = os.path.join(folder_path, new_filename)
         os.rename(new_filepath, new_filepath_renamed)
-        print(f"Skopiowano i zmieniono nazwę pliku {filename} na {new_filepath_renamed}")
+        print(f"Copied and renamed file {filename} to {new_filepath_renamed}")
     else:
-        print(f"Plik {filename} nie istnieje")
+        print(f"File {filename} does not exist")
 
-print("Zakończono")
+print("Finished")
